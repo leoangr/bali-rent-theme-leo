832ee6646bef8671c765d17c1685dafe47aa4a80
leoangr <leoangr12@gmail.com>
2026-05-05 11:39:29 +0700

    update seo

--- header.php
+++ header.php
@@ -5,6 +5,11 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
+    <?php if (is_front_page()) : ?>
+        <meta name="description" content="<?php echo esc_attr(get_bloginfo('description')); ?>">
+    <?php elseif (is_singular()) : ?>
+        <meta name="description" content="<?php echo esc_attr(wp_strip_all_tags(get_the_excerpt())); ?>">
+    <?php endif; ?>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
